9e805bb0919a007993628908ba9831c2ced00448
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-02-28 21:11:41 +0100

    Agregar archivo de configuración de entorno para Firebase

---
+++ environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBOXc8cPOxC3gqjNtQeFWFQmGrYoSIMsk",
+    authDomain: "angular-pocket-links-study.firebaseapp.com",
+    projectId: "angular-pocket-links-study",
+    storageBucket: "angular-pocket-links-study.firebasestorage.app",
+    messagingSenderId: "508130970182",
+    appId: "1:508130970182:web:2c497dc19ff39364d3d847",
+  }
+};
